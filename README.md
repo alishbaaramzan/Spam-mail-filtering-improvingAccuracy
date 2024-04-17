ab868c1fa5eaad252f6805b66f5acff56de61740
alishbaaramzan <wizzz1211@gmail.com>
2024-04-17 21:56:17 +0500

    updated code

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# Spam-mail-filtering
-A text classifier in Python using classification algorithms of machine learning (Support vector machines, Naïve Bayes classifier) to detect if a given mail or message is spam or ham (not spam). Spam dataset was derived from Kaggle, UCI repository etc.   
+#Spam-mail-filtering
+A text classifier in Python using classification algorithms of machine learning (Support vector machines, Naïve Bayes classifier, Random Forest) to detect if a given mail or message is spam or ham (not spam). Spam dataset was derived from Kaggle, UCI repository etc.
 
-E-mail is an effective and very cheap means of communication, this fact is being exploited by numerous organizations for carrying out their advertisements. This process is being carried out so extensively that, email inboxes of millions of people are filled with spam e-mails. This results in lot of wastage of valuable time for each user, to go through the spam emails. Additionally, spam emails traffic between servers results in delay in delivery of important e-mails at the right time to the users. Considering the scale and intensity of this problem, we developed a system that uses the concepts of Multinomial Naïve Bayes theorem and Supports Vector Machines to identify spam e-mails. Our spam detection system identifies emails that are spam emails and in turn saves time for millions of users. 
+E-mail is an effective and very cheap means of communication, this fact is being exploited by numerous organizations for carrying out their advertisements. This process is being carried out so extensively that, email inboxes of millions of people are filled with spam e-mails. This results in a lot of wastage of valuable time for each user, to go through the spam emails. Additionally, spam emails traffic between servers results in a delay in the delivery of important e-mails at the right time to the users. Considering the scale and intensity of this problem, we developed a system that uses the concepts of Multinomial Naïve Bayes theorem, Supports Vector Machines, and Random Forest to identify spam e-mails. Our spam detection system identifies emails that are spam emails and in turn saves time for millions of users.
 
-=========================================================================================
-
-Feel free to comment/suggest changes/Use the code.
-
-ML_FINAL has the complete documentation, background and results of this project.
-
-=========================================================================================
+The Random Forest classifier enhances the accuracy and robustness of the model by aggregating predictions from multiple decision trees. It reduces overfitting compared to individual decision trees and provides better generalization to unseen data. This leads to improved performance in identifying spam emails, resulting in more accurate classification and reduced false positives or negatives. As a result, our spam detection system becomes more reliable and effective in saving users' time and resources.
--- Spam.py
+++ Spam.py
@@ -1,29 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  2 02:21:51 2017
-
-@author: sys
-"""
-
-# import all dependencies
-# -*- coding: utf-8 -*-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score,confusion_matrix,f1_score
+from sklearn.ensemble import RandomForestClassifier  # Import Random Forest classifier
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 
 # read the data and replace null values with a null string
-df1 = pd.read_csv("spamham.csv")
+df1 = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/spamham.csv")
 df = df1.where((pd.notnull(df1)), '')
 
-# Categorize Spam as 0 and Not spam as 1 
-df.loc[df["Category"] == 'ham', "Category",] = 1
-df.loc[df["Category"] == 'spam', "Category",] = 0
-# split data as label and text . System should be capable of predicting the label based on the  text
+# Categorize Spam as 0 and Not spam as 1
+df['Category'] = df['Category'].map({'ham': 1, 'spam': 0})
+# drop rows with missing values in 'Message' column
+df = df.dropna(subset=['Message'])
+
+# split data as label and text. System should be capable of predicting the label based on the text
 df_x = df['Message']
 df_y = df['Category']
+
 # split the table - 80 percent for training and 20 percent for test size
 x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, train_size=0.8, test_size=0.2, random_state=4)
 
@@ -33,36 +28,46 @@ x_trainFeat = tfvec.fit_transform(x_train)
 x_testFeat = tfvec.transform(x_test)
 
 # SVM is used to model
-y_trainSvm = y_train.astype('int')
 classifierModel = LinearSVC()
-classifierModel.fit(x_trainFeat, y_trainSvm)
+classifierModel.fit(x_trainFeat, y_train)
 predResult = classifierModel.predict(x_testFeat)
 
 # GNB is used to model
-y_trainGnb = y_train.astype('int')
 classifierModel2 = MultinomialNB()
-classifierModel2.fit(x_trainFeat, y_trainGnb)
+classifierModel2.fit(x_trainFeat, y_train)
 predResult2 = classifierModel2.predict(x_testFeat)
 
-# Calc accuracy,converting to int - solves - cant handle mix of unknown and binary
-y_test = y_test.astype('int')
-actual_Y = y_test.as_matrix()
+# Random Forest Classifier
+classifierModelRF = RandomForestClassifier(n_estimators=100, random_state=42)
+classifierModelRF.fit(x_trainFeat, y_train)
+predResultRF = classifierModelRF.predict(x_testFeat)
+
+# Calc accuracy, converting to int - solves - cant handle mix of unknown and binary
+actual_Y = y_test.values
 
 print("~~~~~~~~~~SVM RESULTS~~~~~~~~~~")
-#Accuracy score using SVM
-print("Accuracy Score using SVM: {0:.4f}".format(accuracy_score(actual_Y, predResult)*100))
-#FScore MACRO using SVM
-print("F Score using SVM: {0: .4f}".format(f1_score(actual_Y, predResult, average='macro')*100))
-cmSVM=confusion_matrix(actual_Y, predResult)
-#"[True negative  False Positive\nFalse Negative True Positive]"
+# Accuracy score using SVM
+print("Accuracy Score using SVM: {0:.4f}".format(accuracy_score(actual_Y, predResult) * 100))
+# FScore MACRO using SVM
+print("F Score using SVM: {0: .4f}".format(f1_score(actual_Y, predResult, average='macro') * 100))
+cmSVM = confusion_matrix(actual_Y, predResult)
 print("Confusion matrix using SVM:")
 print(cmSVM)
+
 print("~~~~~~~~~~MNB RESULTS~~~~~~~~~~")
-#Accuracy score using MNB
-print("Accuracy Score using MNB: {0:.4f}".format(accuracy_score(actual_Y, predResult2)*100))
-#FScore MACRO using MNB
-print("F Score using MNB:{0: .4f}".format(f1_score(actual_Y, predResult2, average='macro')*100))
-cmMNb=confusion_matrix(actual_Y, predResult2)
-#"[True negative  False Positive\nFalse Negative True Positive]"
+# Accuracy score using MNB
+print("Accuracy Score using MNB: {0:.4f}".format(accuracy_score(actual_Y, predResult2) * 100))
+# FScore MACRO using MNB
+print("F Score using MNB:{0: .4f}".format(f1_score(actual_Y, predResult2, average='macro') * 100))
+cmMNb = confusion_matrix(actual_Y, predResult2)
 print("Confusion matrix using MNB:")
 print(cmMNb)
+
+print("~~~~~~~~~~Random Forest RESULTS~~~~~~~~~~")
+# Accuracy score using Random Forest
+print("Accuracy Score using Random Forest: {0:.4f}".format(accuracy_score(actual_Y, predResultRF) * 100))
+# FScore MACRO using Random Forest
+print("F Score using Random Forest:{0: .4f}".format(f1_score(actual_Y, predResultRF, average='macro') * 100))
+cmRF = confusion_matrix(actual_Y, predResultRF)
+print("Confusion matrix using Random Forest:")
+print(cmRF)
